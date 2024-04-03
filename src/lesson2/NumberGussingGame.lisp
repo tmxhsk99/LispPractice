b875186bf5678abbf998a0096a981d319aa42b3b
juhyeon <tmxhsk99@naver.com>
2024-04-04 08:59:35 +0900

    feat: guess-my-number 함수 정의

--- src/lesson2/NumberGussingGame.lisp
+++ src/lesson2/NumberGussingGame.lisp
@@ -3,5 +3,12 @@ NumberGussingGame:
 - Author: user
 - Date: 2024-04-01
 |#
-(+ 3  (* 2 4))
+#|리스프 전역변수 선언 defparameter 함수|#
+(defparameter *small* 1)
+(defparameter *big* 100)
 
+#|값을 추측 하는 함수|#
+(defun guess-my-number ()
+	(ash (+ *small* *big*) -1))
+
+(print (guess-my-number))
