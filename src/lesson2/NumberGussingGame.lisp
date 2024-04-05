608ebde6721a370647ad5572fa180f8b12015654
juhyeon <tmxhsk99@naver.com>
2024-04-05 09:10:09 +0900

    feat: 1장 내용 작성 완료

--- src/lesson2/NumberGussingGame.lisp
+++ src/lesson2/NumberGussingGame.lisp
@@ -7,8 +7,35 @@ NumberGussingGame:
 (defparameter *small* 1)
 (defparameter *big* 100)
 
-#|값을 추측 하는 함수|#
+#|값을 추측 하는 함수 나누기 2 한다음 소수점 버림|#
 (defun guess-my-number ()
 	(ash (+ *small* *big*) -1))
 
-(print (guess-my-number))
+
+(defun smaller ()
+  (setf *big* (1- (guess-my-number)))
+  (guess-my-number))
+
+(defun bigger ()
+  (setf *small* (1+ (guess-my-number)))
+  (guess-my-number))
+
+(defun start-over()
+  (defparameter *small* 1)
+  (defparameter *big* 100)
+  (guess-my-number))
+
+#|flet 예시|#
+(print (flet ((f (n)
+        (+ n 10))
+       (g (n)
+        (- n 3)))
+ (g (f 5))))
+
+#|labels 예시|#
+(print (labels ((a (n)
+            (+ n 5))
+         (b (n)
+            (+ (a n) 6)))
+  (b 10)))
+
